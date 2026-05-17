08b7a713215e49808a3b14de4fd35c3e96b20fa7
NightJackal <grahamrybarczyk@gmail.com>
2026-05-16 23:44:39 -0400

    Improve file input handling in TORUS lexer
    Refactor file handling and user input for .torus files.

--- TORUSlexer.cpp
+++ TORUSlexer.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-/*
-This is the Cpp lexer for the TORUS compiler (We chose to use the more complex compiler over
-an interpreter for speed).
-15 May 2025
-*/
+
 int main()
 {
+    using std::cin;
     using std::cout;
-    std::ifstream tests("tests.torus");
-    std::string line;
-    if (tests.is_open())
-    {
-        cout << "The file is properly connected\n";
-    }
-    else
-    {
-        cout << "The file is not connected\n";
-        return 1;
-    };
+    using std::string;
+    string src_code;
+    string file_directory;
+    
+    cout << "Please provide the directory for your .torus file (OS Sensitive):\n";
+    cin >> file_directory;
+    std::ifstream file(file_directory);
 
-    int lineNum = 0;
+    if (!file.is_open())
+    {
+        cout << "The File has not been found, please try again...\n";
+        cin >> file_directory;
+        std::ifstream file(file_directory);
+        if (!file.is_open())
+        {
+            cout << "File not found. Do you have TORUS raw source code for compiling(Yes or No)?\n";
+            cin >> src_code;
+        }
+    }
+    if (file.is_open())
+    {
+        cout << "The file has been found\n";
+    }
+
+    std::string line;
+    int line_num = 0;
+    cout << "Entering While Loop\n";
+    cout << "File size check: " << file.tellg() << "\n";
+    while (std::getline(file, line))
+    {
+        cout << "Inside while loop";
+        line_num++;
+        cout << line << '\n';
+        string first_line = line;
+        int i = 0;
+        while (i < first_line.length())
+        {
+
+            i++;
+        }
+    }
+    cout << "While loop ended\n";
+
+    cout << "Operation Executed\n";
     return 0;
 }
