7e5ec8d2ec1af343483fbaab591a97eb7efaa490
NightJackal <grahamrybarczyk@gmail.com>
2026-05-20 00:12:55 -0400

    Developing TORUS Lexer and Parser

--- TORUSlexer.cpp
+++ TORUSlexer.cpp
@@ -1,44 +1,66 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
+#include <vector>
 
-int main()
-{
-    using std::cin;
+int main (){
+#pragma region usings
     using std::cout;
+    using std::cin;
     using std::string;
+    using std::getline;
+    using std::ifstream;
+    #pragma endregion
+#pragma region file_opener
 
-#pragma region file opener
-
-    string file_directory = "/Users/graham/Desktop/TORUS/tests.torus"; // Sets File Directory
-    std::ifstream file(file_directory);                                // Opens File
-    if (file.is_open())                                                // checks if the file is open
-    {
-        cout << "File Succesfully Opened\n\n";
+    string file_directory = "/Users/graham/Desktop/TORUS/tests.torus";//Stes file directory
+    ifstream file(file_directory);//Opens file
+    if (file.is_open()){
+        cout << "\nFile Succesfully Opened\n";
     }
-    if (!file.is_open()) // if the file was not found, declares it and stops program
-    {
-        cout << "File Not Found\n";
+    if (!file.is_open()){
+        cout << "File not found...";
         return 1;
     }
 
-    int line_num = 0;
-    string line;
-    while (std::getline(file, line))
+    #pragma endregion
+#pragma region file_reader
+
+     int line_num = 0; // Sets the line number to zero so it can be incremented in the while loop
+     std::vector<int> tokenization;   
+     std::vector<string> tokis;           
+    string line;                     // Declares a string called line that will be used to store each line from tests.torus
+    while (std::getline(file, line)) // Reads the line and then loops the process below as well as continuing to read tests.torus
     {
-        line_num++;
-        cout << "Line " << line_num << ":\n"
-             << line << "\n\n";
+        line_num++; // Increments line_num in orcer to move onto the next line
+        for (int i = 0; i < line.length(); i++)
+        {
+            char current_char = line[i];
+            if(isdigit(current_char)){
+                tokenization.push_back(1);
+                tokis.push_back();
+            }
+            
+           else if(isalpha(current_char)){
+                tokenization.push_back(3);
+            }
+            
+            else if(isspace(current_char)){
+                tokenization.push_back(5);
+            }
+            else{
+                tokenization.push_back(4);
+            }
+            
+        }
+        for( int i2 = 0; i2 < tokenization.size(); i2++){
+            cout << tokenization[i2];
+        }
+        cout << "\n";
     }
-#pragma endregion
-
-#pragma region reader
-
-#pragma endregion
-
-#pragma region processer
-
-#pragma endregion
+    #pragma endregion
+#pragma region tokenizer
+    #pragma endregion
 
     return 0;
 }
