04fc6bbe8c1ba1c114395b7799937346e573b6ca
NightJackal <grahamrybarczyk@gmail.com>
2026-06-20 23:57:31 -0400

    Add token_library.h with symbol and keyword vectors

---
+++ Compiler Stages/stage_two/token_library.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+    std::vector<std::string> symbols_vector = {";", "+", "-", "*", "÷", "=", "/*", "*/"};
+    std::vector<int> symbols_DTorCNs_vector = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<std::string> keywords_vector = {"citizen", "num", "char", "string"};
+    std::vector<int> keywords_DTorCNs_vector = {1, 2, 3, 4};
+    std::vector<std::string> variables_vector = {};
+    std::vector<int> var_DTorCDs_vector = {};
+    std::vector<double> var_value_vector = {};
+    std::vector<int> var_value_DTorCNs_vector = {};
